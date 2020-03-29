fe48c57ee47919d6f97b39f54d5670eec6b40d97
Joel Lobo <joellobo@gmail.com>
2020-03-29 20:28:59 -0300

    Add others exercices

---
+++ stock-trader/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+	<v-app>
+		<Header />
+		<v-content>
+			<router-view></router-view>
+		</v-content>
+	</v-app>
+</template>
+
+<script>
+import Header from './components/Header'
+
+export default {
+  components: { Header },
+  created() {
+	  this.$store.dispatch('initStocks')
+  }
+}
+</script>
+
+<style>
+
+</style>
+
